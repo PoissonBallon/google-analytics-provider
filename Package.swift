99834fe67f9fb607ab65db3fc14175cda424fa11
Allan Vialatte <allan.vialatte@icloud.com>
2018-09-02 13:49:21 +0200

    [WIP]

--- Package.swift
+++ Package.swift
@@ -4,25 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "GoogleAnalyticsProvider",
-    products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "GoogleAnalyticsProvider",
-            targets: ["GoogleAnalyticsProvider"]),
+  name: "GoogleAnalyticsProvider",
+  products: [
+    .library(name: "GoogleAnalyticsProvider", targets: ["GoogleAnalyticsProvider"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "GoogleAnalyticsProvider",
-            dependencies: []),
-        .testTarget(
-            name: "GoogleAnalyticsProviderTests",
-            dependencies: ["GoogleAnalyticsProvider"]),
+  dependencies: [
+    .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
+  ],
+  targets: [
+    .target(name: "GoogleAnalyticsProvider", dependencies: []),
+    .testTarget(name: "GoogleAnalyticsProviderTests", dependencies: ["GoogleAnalyticsProvider"]),
     ]
 )
--- Sources/GoogleAnalyticsProvider/GoogleAnalyticsProvider.swift
+++ Sources/GoogleAnalyticsProvider/GoogleAnalyticsProvider.swift
@@ -1,3 +1,32 @@
-struct GoogleAnalyticsProvider {
-    var text = "Hello, World!"
+//
+//  GoogleAnalyticsProvider.swift
+//  App
+//
+//  Created by Allan Vialatte on 25/07/2018.
+//
+
+import Foundation
+import Vapor
+
+
+
+public final class GoogleAnalyticsProvider: Provider {
+
+  public init() { }
+  
+  public func boot(_ worker: Container) throws { }
+  
+  public func didBoot(_ worker: Container) throws -> EventLoopFuture<Void> {
+    return .done(on: worker)
+  }
+  
+  public func register(_ services: inout Services) throws {
+    services.register { (container) -> GoogleAnalyticsClient in
+      let subContainer = container.subContainer(on:  MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount))
+      let httpClient = try subContainer.make(Client.self)
+      let config = try subContainer.make(GoogleAnalyticsConfig.self)
+      let logger = try subContainer.make(Logger.self)
+      return GoogleAnalyticsClient(config: config, client: httpClient, logger: logger)
+    }
+  }
 }
